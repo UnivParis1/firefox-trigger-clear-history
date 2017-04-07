631d175ff2b90c35e9c3878aa42d8577c0af24e8
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-04-07 14:12:45 +0200

    migrate to WebExtension

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
+function extrapolateUrlFromCookie(cookie) {
+    var prefix = cookie.secure ? "https://" : "http://";
+    if (cookie.domain.charAt(0) == ".")
+        prefix += "www";
 
-const cookieRegexTriggeringClearHistory = /^forceBrowserExit$/;
-const urlRegexTriggeringClearHistory = ''; // /^https:\/\/cas.univ-paris1.fr\/cas\/logout/;
-
-let {Cc, Ci} = require("chrome");
-let cookieManager = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager);
-var browserHistory = Cc["@mozilla.org/browser/nav-history-service;1"].getService(Ci.nsIBrowserHistory);
-let observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-
-
-function clearHistory() {
-    cookieManager.removeAll();
-    browserHistory.removeAllPages();
+    return prefix + cookie.domain + cookie.path;
+}
+function removeCookie(cookie) {
+    browser.cookies.remove({ url: extrapolateUrlFromCookie(cookie), name: cookie.name })
 }
 
-if (urlRegexTriggeringClearHistory) {
-    let obs = {
-	observe: function (aSubject, aTopic, aData) {
-            let httpChannel = aSubject.QueryInterface(Ci.nsIHttpChannel);
-            let requestUrl = httpChannel.URI.spec;
-	    if (requestUrl.match(urlRegexTriggeringClearHistory)) {
-		console.info('clearing history&cookies since url is ' + requestUrl);
-		clearHistory();
-	    }
-	},
-	reg: function () {
-            observerService.addObserver(obs, 'http-on-modify-request', false);
-	},
-	unreg: function () {
-            observerService.removeObserver(obs, 'http-on-modify-request');
-	}
-    };
-    obs.reg();
+function removeCookies() {
+  browser.cookies.getAll({}).then((cookies) => {
+     for (cookie of cookies) removeCookie(cookie);
+  });
 }
 
-if (cookieRegexTriggeringClearHistory) {
-    let obs = {
-	observe: function (aSubject, aTopic, aData) {
-	    if (aData === "added") {
-		let cookie = aSubject.QueryInterface(Ci.nsICookie2);
-		if (cookie.name.match(cookieRegexTriggeringClearHistory)) {
-		    console.info('clearing history&cookies since cookie is ' + cookie.name);
-		    clearHistory();
-		}
-	    }
-	},
-	reg: function () {
-            observerService.addObserver(obs, 'cookie-changed', false);
-	},
-	unreg: function () {
-            observerService.removeObserver(obs, 'cookie-changed');
-	}
-    };
-    obs.reg();
-}
+browser.cookies.onChanged.addListener(function(changeInfo) {
+    if (changeInfo.cookie && changeInfo.cookie.name === "forceBrowserExit") {
+        browser.history.deleteAll();
+        removeCookies();
+    }
+});
